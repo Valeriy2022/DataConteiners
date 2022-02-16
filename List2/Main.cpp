025f763fde06f0bcdf60a6f49188a1eebed2c37c
Valeriy2022 <5474118@gmail.com>
2022-02-16 18:15:38 +0300

    HW. Erase DONE. Refactoring DONE.

--- List2/Main.cpp
+++ List2/Main.cpp
@@ -30,6 +30,49 @@ class List
 	/*Element* Head;
 	Element* Tail;
 	unsigned int size;*/
+
+	class Iterator
+	{
+		Element* Temp;
+	public:
+		Iterator(Element* Temp = nullptr) :Temp(Temp)
+		{
+			cout << "ItConstructor:\t" << this << endl;
+		}
+		~Iterator()
+		{
+			cout << "ItDestructor:\t" << this << endl;
+		}
+
+		bool operator==(const Iterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const Iterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+		Iterator& operator++()	//Prefix increment
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		Iterator operator++(int)//Postfix increment
+		{
+			Iterator old = *this;	
+			Temp = Temp->pNext;		
+			return old;	
+		}
+		const int& operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+
+	};
 public:
 	List()
 	{
@@ -37,9 +80,29 @@ public:
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+
+	List(const initializer_list<int>& il) :List()
+	{		
+		cout << typeid(il.begin()).name() << endl;		
+		for (int const* it = il.end() - 1; it != il.begin() - 1; it--)
+		{
+			push_front(*it);
+		}
+	}
+
 	~List()
 	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
 		cout << "LDestructor:\t" << this << endl;
+	}
+	Iterator begin()
+	{
+		return Head;
+	}
+	Iterator end()
+	{
+		return nullptr;
 	}
 
 	//			Adding elements:
@@ -51,11 +114,11 @@ public:
 			size++;
 			return;
 		}
-		Element* New = new Element(Data);
+		/*Element* New = new Element(Data);
 		New->pNext = Head;
 		Head->pPrev = New;
-		Head = New;
-		//Head = Head->pPrev = new Element(Data, Head);
+		Head = New;*/
+		Head = Head->pPrev = new Element(Data, Head);
 		size++;
 	}
 	void push_back(int Data)
@@ -66,11 +129,104 @@ public:
 			size++;
 			return;
 		}
-		Element* New = new Element(Data);
-		New->pPrev = Tail;
-		Tail->pNext = New;
-		Tail = New;
+		//Element* New = new Element(Data);//1)
+		//New->pPrev = Tail;//2)
+		//Tail->pNext = New;//3)
+		//Tail = New;//4)
+		Tail = Tail->pNext = new Element(Data,0,Tail);
 		size++;
+	}
+	void insert(int index, int Data)
+	{
+		if (index > size)return;
+		if (index == 0)return push_front(Data);
+		if (index == size)return push_back(Data);
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - 1 - index; i++)Temp = Temp->pPrev;
+		}
+		//Неважно, с какой стороны мы добрались до нужного элемента, 
+		//процедура добавления нового элемента быдет идентичной:
+		//Element* New = new Element(Data);
+		//New->pNext = Temp;//1)
+		//New->pPrev = Temp->pPrev;//2)
+		//Temp->pPrev->pNext = New;//3)
+		//Temp->pPrev = New;//4)
+
+		Temp->pPrev = Temp->pPrev->pNext = new Element(Data, Temp, Temp->pPrev);
+		size++;
+	}
+
+	//			Removing elements:
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		Head = Head->pNext;
+		delete Head->pPrev;
+		Head->pPrev = nullptr;
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == Tail)return pop_front();
+		Tail = Tail->pPrev;
+		delete Tail->pNext;
+		Tail->pNext = nullptr;
+		size--;
+	}
+	void erase(int index)
+	{
+		if (index > size)return;
+		if (Head == nullptr && Tail == nullptr) return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		if (index == 0)
+		{			
+			pop_front();
+			return;
+		}
+
+	    if (index == size)
+		{
+			pop_back();			
+			return;
+		}
+
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - 1 - index; i++)Temp = Temp->pPrev;
+		}
+		Temp->pPrev->pNext = Temp->pNext;
+		Temp->pNext->pPrev = Temp->pPrev;
+		delete Temp;
+		size--;
+		
 	}
 
 	//			Methods:
@@ -88,29 +244,80 @@ public:
 	}
 };
 
+//#define BASE_CHECK
+#define RANGE_BASE_FOR_LIST
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
 	List list;
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
 	list.print();
-	/*list.reverse_print();*/
+	list.reverse_print();
+    list.push_front(555);
+	list.print();
 	list.push_back(555);
 	list.print();
+
+
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(index-1, value);
+	list.print();
+	
+	list.reverse_print();
+
+	int index;
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	list.erase(index-1);
+	list.print();
+	list.reverse_print();
+#endif // BASE_CHECK
+
+#ifdef RANGE_BASE_FOR_LIST
+	List list = { 3,5,8,13,21 };
+	for (int i : list)
+	{
+		cout << i << tab;
+	}
+	cout << endl;
+#endif // RANGE_BASE_FOR_LIST
+	
 }
 
+//void main()
+//{
+//	setlocale(LC_ALL, "");
+//	int n;
+//	cout << "Введите размер списка: "; cin >> n;
+//	List list;
+//	for (int i = 0; i < n; i++)
+//	{
+//		list.push_front(rand() % 100);
+//	}
+//	list.print();
+//	/*list.reverse_print();*/
+//	list.push_back(555);
+//	list.print();
+//	list.push_front(555);
+//	list.print();
+//}
+
 //TODO:
-//Добавить в список следующие методы :
-//void push_back(int Data);
-//void pop_front();
-//void pop_back();
-//void insert(int index, int Data);
-//void erase(int index);
+
+//Проверочный код должен заработать:
+//List list = { 3, 5, 8, 13, 21 };
+//for (int i : list)cout << i << tab; cout << endl;
+
 //
 //Destructor;
 //CopyMethods;
